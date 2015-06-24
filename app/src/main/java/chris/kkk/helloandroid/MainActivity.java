62156cb2fc9e0048e46db768ab0ec4aa7e102b4c
jungjunghsiao <jungjunghsiao1228@gmail.com>
2015-06-24 15:30:57 +0800

    test push

--- app/src/main/java/chris/kkk/helloandroid/MainActivity.java
+++ app/src/main/java/chris/kkk/helloandroid/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //test push
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
