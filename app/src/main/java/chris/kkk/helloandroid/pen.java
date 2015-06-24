de21756599e52fc3ab0a60cb4880afd0ea87db19
jungjunghsiao <jungjunghsiao1228@gmail.com>
2015-06-24 15:11:21 +0800

    Initial commit

---
+++ app/src/main/java/chris/kkk/helloandroid/pen.java
@@ -0,0 +1,34 @@
+package chris.kkk.helloandroid;
+
+public class pen {
+
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public pen(String name, int price) {
+        this.name = name;
+        this.price = price;
+
+
+    }
+
+
+
+
+}
